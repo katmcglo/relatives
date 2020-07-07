c9b4288c213a0fab15fd7428cecc493fc761e338
Dareos Khalili <khalilidareos@gmail.com>
2020-07-07 19:44:51 +0200

    redesigns homepage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,12 @@
 class PagesController < ApplicationController
   def home
     @featured_relatives = FamilyMember.where(featured: true)
+    @brunhilde = FamilyMember.where(name: "Brünhilde Meier")
   end
 
   def show
     render template: "pages/#{params[:page]}"
   end
 
-  
+
 end
